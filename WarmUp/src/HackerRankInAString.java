7c4fb8a57b9dfcdc0187a01a2dcb979fe6f32fd8
Mohit Amgaonkar <mohithamgaonkar@gmail.com>
2023-12-31 18:36:32 -0500

    Added solution for 2DArrayDS and HackerRankInAString

---
+++ WarmUp/src/HackerRankInAString.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    private static String HACKRANK_STRING = "hackerrank";
+    /*
+     * Complete the 'hackerrankInString' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts STRING s as parameter.
+     */
+
+    public static String hackerrankInString(String s) {
+        int index=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i) == HACKRANK_STRING.charAt(index)){
+                index++;
+            }
+            if(index == HACKRANK_STRING.length()){
+                return "YES";
+            }
+        }
+        return "NO";
+    }
+
+}
+
+public class HackerRankInString {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                String result = Result.hackerrankInString(s);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
